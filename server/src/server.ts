e266d37fcb343c9b358db37d3430218dd6240f6a
Matheus Vinicius <57428641+matheus778@users.noreply.github.com>
2023-01-24 20:14:19 -0300

    projeto finalizado

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors';
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./lib/routes";
+
+const app = Fastify();
+
+app.register(cors);
+app.register(appRoutes);
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('server running')
+});
